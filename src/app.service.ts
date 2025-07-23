402caa7bce0d50d0ab519c6a1fb1a33330cb6148
Zedline12 <bedomohamed307@gmail.com>
2025-07-23 21:11:54 +0300

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -28,31 +28,32 @@ export class AppService {
     const inboxMessages =
       await this.imapService.getInboxMessages(searchCriteria);
     console.log(users, inboxMessages);
-    users.forEach(async (user) => {
-      if (!inboxMessages.includes(user.email)) {
-        if (moment(user.createdAt).isBefore(fourDaysAgo)) {
-          console.log('SENT EMAIL AND REMOVE');
-          await this.mailService.sendMail(
-            user.email,
-            'test',
-            'test',
-            'no-reply-template',
-            { firstName: user.firstName },
-          );
-          await fetch('https://hooks.zapier.com/hooks/catch/22809533/uuy6dt0/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email: user.email }),
-          });
-          await this.userModel.deleteOne({ email: user.email });
-        } else {
-          console.log('DOSENT SENT ,The date is less than 4 days old');
-        }
-      } else {
-        console.log('USER ACTUALLY REPLIED');
-        await this.userModel.deleteOne({ email: user.email });
-      }
-    });
+    for (const user of users) {
+  if (!inboxMessages.includes(user.email)) {
+    if (moment(user.createdAt).isBefore(fourDaysAgo)) {
+      console.log('SENT EMAIL AND REMOVE');
+      await this.mailService.sendMail(
+        user.email,
+        'test',
+        'test',
+        'no-reply-template',
+        { firstName: user.firstName },
+      );
+      await fetch('https://hooks.zapier.com/hooks/catch/22809533/uuy6dt0/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: user.email }),
+      });
+      await this.userModel.deleteOne({ email: user.email });
+    } else {
+      console.log('DOESN’T SEND, date is less than 4 days old');
+    }
+  } else {
+    console.log('USER ACTUALLY REPLIED');
+    await this.userModel.deleteOne({ email: user.email });
+  }
+}
+
   }
   async createUser(user: User) {
     return await this.userModel.create(user);
